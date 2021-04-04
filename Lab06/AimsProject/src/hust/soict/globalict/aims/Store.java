a060d1da4b5bf2140ea8b933a025899c0cef0187
AnhVu <vu25ck28@gmail.com>
2021-04-04 21:54:00 +0700

    Version of Lab 06

--- Lab06/AimsProject/src/hust/soict/globalict/aims/Store.java
+++ Lab06/AimsProject/src/hust/soict/globalict/aims/Store.java
@@ -1,34 +1,27 @@
 package hust.soict.globalict.aims;
 
-import hust.soict.globalict.aims.disc.DigitalVideoDisc;
+import hust.soict.globalict.aims.media.*;
 
 import java.util.Vector;
 
 public class Store {
     public static final int MAX = 20;
-    private static Vector <DigitalVideoDisc> store = new Vector<>(MAX);
-    public void addDVDs (DigitalVideoDisc disc) {
-        if (store.size() < MAX) {
-            store.add(disc);
+    private static Vector<Media> cart = new Vector<>(MAX);
+    public void addMedia (Media media) {
+        if (cart.size() < MAX) {
+            cart.add(media);
             System.out.println("The disc has been added");
         }
         else
             System.out.println("The cart is full");
     }
 
-    public void removeDVDs (DigitalVideoDisc disc) {
-        for (int i = 0; i < store.size(); i++) {
-            if (store.get(i).getTitle().compareTo(disc.getTitle()) == 0) {
-                store.remove(i);
+    public void removeMedia (Media media) {
+        for (int i = 0; i < cart.size(); i++) {
+            if (cart.get(i).getTitle().compareTo(media.getTitle()) == 0) {
+                cart.remove(i);
                 System.out.println("Remove successfully");
             }
-        }
-    }
-
-    public void displayStore () {
-        System.out.println("DVDs in cart: ");
-        for (DigitalVideoDisc disc : store) {
-            disc.getInfo();
         }
     }
 }
