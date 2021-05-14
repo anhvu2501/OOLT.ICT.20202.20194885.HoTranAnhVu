9937afa4c224290a81cc9a08dca1ab7385d93a48
AnhVu <vu25ck28@gmail.com>
2021-05-14 13:59:25 +0700

    I have to config my javaFx and SceneBuilder So I 've just finished to set up again the environment and sdk

---
+++ Lab10/AimsProject/src/hust/soict/globalict/aims/disc/DigitalVideoDisc.java
@@ -0,0 +1,24 @@
+package hust.soict.globalict.aims.disc;
+
+import hust.soict.globalict.aims.Playable;
+import hust.soict.globalict.aims.media.Disc;
+
+import java.time.LocalDate;
+
+public class DigitalVideoDisc extends Disc implements Playable{
+    private static int nbDigitalVideoDisc = 0;
+    private LocalDate dateAdded;
+
+    public LocalDate getDateAdded () {
+        return this.dateAdded;
+    }
+
+    public DigitalVideoDisc (int id, String title, String category, double cost, int length, String director) {
+        super (id, title, category, cost, length, director);
+    }
+
+    public void play() {
+        System.out.println("Playing DVD: " + this.getTitle());
+        System.out.println("DVD length: " + this.getLength());
+    }
+}
