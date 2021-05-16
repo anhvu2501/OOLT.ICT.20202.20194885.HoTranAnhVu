6d2c240ae420c1ace80f2c27ee98617b661fd794
anhvu2501 <vu25ck28@gmail.com>
2021-05-16 16:41:10 +0700

    Problem 1 finished - drawing by activating Pen Button

--- Lab10/GUIProject/src/hust/soict/globalict/javafx/PainterController.java
+++ Lab10/GUIProject/src/hust/soict/globalict/javafx/PainterController.java
@@ -1,25 +1,45 @@
 package hust.soict.globalict.javafx;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class PainterController {
     @FXML
-    private VBox drawingAreaPane;
+    private Pane drawingAreaPane;
 
     @FXML
-    void clearButtonPressed(ActionEvent event) {
+    private RadioButton eraser;
+
+    @FXML
+    private RadioButton pen;
+
+    @FXML
+    public void clearButtonPressed(ActionEvent event) {
         drawingAreaPane.getChildren().clear();
     }
 
     @FXML
-    void drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
-        drawingAreaPane.getChildren().add(newCircle);
+    public void drawingAreaMouseDragged(MouseEvent event) {
+        if (pen.isSelected()) {
+            Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
+            drawingAreaPane.getChildren().add(newCircle);
+        }
+        else {
+
+        }
+        if (eraser.isSelected()) {
+            Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.WHITE);
+            drawingAreaPane.getChildren().add(newCircle);
+        }
+        else {
+
+        }
     }
 
 }
